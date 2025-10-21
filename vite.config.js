eaddf8351d0f3c1348d12e86c108847537e929a0
ShadowyStudent <poasanrevolucion@hotmail.com>
2025-10-20 21:10:15 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import { ghPages } from 'vite-plugin-gh-pages'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), ghpages()],
+  plugins: [vue(), ghPages()],
   base: "SPA-CRUD"
 })
