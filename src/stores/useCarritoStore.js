ca889eb293c5f0812ed2fc6038585ae6c2f20437
Miguel Álvarez <alteregoreborn@gmail.com>
2025-10-20 21:17:12 -0600

    Carrito

--- src/stores/useCarritoStore.js
+++ src/stores/useCarritoStore.js
@@ -1,34 +1,37 @@
 ﻿import { defineStore } from 'pinia'
-import { ref, computed } from 'vue'
 
-export const useCarritoStore = defineStore('carrito', () => {
-  const carrito = ref([])
-
-  function agregarAlCarritoCantidad(producto, cantidad = 1) {
-    const id = Number(producto.id)
-    const existente = carrito.value.find(p => p.id === id)
-    if (existente) existente.cantidad += Number(cantidad)
-    else carrito.value.push({ id, name: producto.name, price: Number(producto.price), cantidad: Number(cantidad) })
+export const useCarritoStore = defineStore('carrito', {
+  state: () => ({
+    items: JSON.parse(localStorage.getItem('carrito') || '[]')
+  }),
+  getters: {
+    totalItems: (state) => state.items.reduce((s, i) => s + i.cantidad, 0),
+    totalPrecio: (state) => state.items.reduce((s, i) => s + i.cantidad * i.precio, 0)
+  },
+  actions: {
+    guardar() {
+      localStorage.setItem('carrito', JSON.stringify(this.items))
+    },
+    agregar(item) {
+      const found = this.items.find(i => i.productId === item.productId)
+      if (found) found.cantidad += item.cantidad
+      else this.items.push({ ...item })
+      this.guardar()
+    },
+    actualizarCantidad(productId, cantidad) {
+      const it = this.items.find(i => i.productId === productId)
+      if (!it) return
+      it.cantidad = cantidad
+      if (it.cantidad <= 0) this.items = this.items.filter(i => i.productId !== productId)
+      this.guardar()
+    },
+    eliminar(productId) {
+      this.items = this.items.filter(i => i.productId !== productId)
+      this.guardar()
+    },
+    vaciar() {
+      this.items = []
+      this.guardar()
+    }
   }
-
-  function agregarAlCarrito(producto) { agregarAlCarritoCantidad(producto, 1) }
-
-  function eliminarDelCarrito(id) {
-    const i = carrito.value.findIndex(p => p.id === Number(id))
-    if (i !== -1) carrito.value.splice(i, 1)
-  }
-
-  function cambiarCantidad(id, nuevaCantidad) {
-    const item = carrito.value.find(p => p.id === Number(id))
-    if (!item) return
-    item.cantidad = Number(nuevaCantidad)
-    if (item.cantidad <= 0) eliminarDelCarrito(id)
-  }
-
-  function vaciarCarrito() { carrito.value.splice(0, carrito.value.length) }
-
-  const totalArticulos = computed(() => carrito.value.reduce((s, p) => s + (p.cantidad || 0), 0))
-  const totalPrecio = computed(() => carrito.value.reduce((s, p) => s + (p.price * (p.cantidad || 1)), 0))
-
-  return { carrito, agregarAlCarrito, agregarAlCarritoCantidad, eliminarDelCarrito, cambiarCantidad, vaciarCarrito, totalArticulos, totalPrecio }
 })
